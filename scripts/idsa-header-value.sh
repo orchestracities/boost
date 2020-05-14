9187756455deb71ad5b2389bfdccc65487ca3d81
Andrea Falconi <andrea.falconi@gmail.com>
2020-05-14 15:14:21 +0200

    fix manual jwt end2end testing.

--- scripts/idsa-header-value.sh
+++ scripts/idsa-header-value.sh
@@ -15,7 +15,7 @@ base64 <<EOT
 	"id": "http://industrialdataspace.org/queryMessage/1a421b8c-3407-44a8-aeb9-253f145c869a",
 	"issued": "2019-10-25T15:35:34.589Z",
 	"modelVersion": "2.1.0",
-	"issuerConnector": "https://companyA.com/connector/59a68243-dd96-4c8d-88a9-0f0e03e13b1b",
+	"issuerConnector": "https://companyA.com/connector/4e16f007-d959-4eb2-b47d-78dd0c4eab0e",
 	"securityToken": {
 		"@type": "ids:DynamicAttributeToken",
 		"tokenFormat": "https://w3id.org/idsa/code/tokenformat/JWT",
